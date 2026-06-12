68235d400e6aacccfbfd1a9f2e6d42b652f44562
fghjcvbfgh-cyber <fghjcvbfgh@gmail.com>
2026-06-12 15:19:01 +0300

    Update protection_bot.py

--- protection_bot.py
+++ protection_bot.py
@@ -234,4 +234,4 @@ async def cmd_warns(ctx, member: discord.Member = None):
 async def on_ready():
     print(f"✅  [{SERVER_NAME}] Protection Bot شغّال: {bot.user}")
 
-bot.run(MTUxNDk2MDE1MzQzMzA4NDA0NQ.GQ5--K.imu6awY4ZxMkaXnjP2j2GX-ve0E4xgBq55xenQ)
+bot.run(os.getenv('DISCORD_TOKEN'))
